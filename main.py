babaea62917093537697b552a1e8471ad07fa2a5
MrRedaM <reda.morsli.1@ens.etsmtl.ca>
2023-10-11 19:28:36 -0300

    Implement microservice architecture

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-interface = "cni0"
-frequency = 15
 pcap_source_folder = "/nfs/pcap/"
 ip_address = '192.168.56.5'
 port = '30200'
@@ -16,7 +14,7 @@ port = '30200'
 def send_pcap(file_path, url):
     try:
         with open(file_path, 'rb') as pcap_file:
-            response = requests.post(url, files={'pcap_file': pcap_file})
+            response = requests.post(url, files={'file': pcap_file})
             if response.status_code == 200:
                 print(f"PCAP file {file_path} successfully sent.")
             else:
@@ -31,16 +29,16 @@ class PCAPFileHandler(FileSystemEventHandler):
         if event.is_directory:
             return
         else:
-            source_file = os.path.join(pcap_source_folder, event.src_path)
-            send_pcap(source_file, f"http://{ip_address}:{port}/analyse-pcap/")
+            # source_file = os.path.join(pcap_source_folder, event.src_path)
+            send_pcap(event.src_path, f"http://{ip_address}:{port}/analyse-pcap/")
 
     def on_modified(self, event):
         print("PCAP file modified: " + event.src_path)
         if event.is_directory:
             return
         else:
-            source_file = os.path.join(pcap_source_folder, event.src_path)
-            send_pcap(source_file, f"http://{ip_address}:{port}/analyse-pcap/")
+            # source_file = os.path.join(pcap_source_folder, event.src_path)
+            send_pcap(event.src_path, f"http://{ip_address}:{port}/analyse-pcap/")
 
 
 if __name__ == "__main__":
@@ -49,8 +47,6 @@ if __name__ == "__main__":
     pcap_observer = Observer()
     pcap_observer.schedule(pcap_event_handler, pcap_source_folder, recursive=False)
     pcap_observer.start()
-
-    subprocess.run(f"tcpdump -i {interface} -w /nfs/pcap/capture-%Y-%m-%d_%H:%M:%S.pcap -G {frequency}")
 
     while True:
         time.sleep(1)
